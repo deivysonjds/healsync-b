ca4884867906ea7cce465b3e8255e0ea79c2d983
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-05-11 11:38:23 -0300

    commit initial

---
+++ healsync/src/main/java/com/pi/healsync/HealsyncApplication.java
@@ -0,0 +1,13 @@
+package com.pi.healsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealsyncApplication.class, args);
+	}
+
+}
