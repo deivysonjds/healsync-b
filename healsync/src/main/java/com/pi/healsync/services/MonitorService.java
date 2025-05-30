97a7da5b493a76e8ac7b5ee7f770dc58f63276ec
Lucas Menezes Zeltser <163359334+lucasmenezeszeltser@users.noreply.github.com>
2025-05-29 23:55:01 -0300

    Create MonitorService.java

---
+++ healsync/src/main/java/com/pi/healsync/services/MonitorService.java
@@ -0,0 +1,39 @@
+package com.pi.healsync.services;
+import java.util.UUID;
+import java.util.Optional;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pi.healsync.exceptions.NoSuchException;
+import com.pi.healsync.exceptions.ObjectNotCreated;
+import com.pi.healsync.models.Monitor;
+import com.pi.healsync.repositories.MonitorRepository;
+
+@Service
+  public class MonitorService {
+
+@Autowired
+    private MonitorRepository monitorRepository;
+
+@Transactional
+public Monitor insert(Monitor monitor){
+
+  try {
+            return monitorRepository.save(monitor);
+        } catch (Exception e) {
+            throw new ObjectNotCreated(e);
+        }
+    }
+
+  @Transactional
+    public Monitor findById(UUID id){
+    f (!monitor.isPresent()) {
+            throw new NoSuchException("Monitor");
+        }
+
+        return monitor.get();
+
+    }
+  }
