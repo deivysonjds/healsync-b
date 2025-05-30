38b127c7167deb15d8fba80e835c1c75e0dc3bb3
Lucas Menezes Zeltser <163359334+lucasmenezeszeltser@users.noreply.github.com>
2025-05-29 23:39:06 -0300

    Create MonitorRequestDTO.java

---
+++ healsync/src/main/java/com/pi/healsync/DTO/MonitorRequestDTO.java
@@ -0,0 +1,20 @@
+package com.pi.healsync.DTO;
+
+import com.pi.healsync.models.Monitor;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MonitorRequestDTO {
+    private long numeroTombamento;
+   
+
+    public MonitorRequestDTO(){}
+
+    public MonitorRequestDTO(Monitor monitor){
+      numeroTombamento = monitor.getNumeroTombamento();
+        
+    }
+}
