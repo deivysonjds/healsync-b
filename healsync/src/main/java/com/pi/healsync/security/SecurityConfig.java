3396c16613c7aae862c118bf9e957876b3c1d49c
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-06-08 01:02:40 -0300

    config: cors -> added domain

--- healsync/src/main/java/com/pi/healsync/security/SecurityConfig.java
+++ healsync/src/main/java/com/pi/healsync/security/SecurityConfig.java
@@ -56,7 +56,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
             "http://localhost:*",
-            "https://healsync-f.vercel.app"
+            "https://healsync-f.vercel.app",
+            "healsync-f-deivyson-silvas-projects.vercel.app"
         ));
 
         config.setAllowedMethods(List.of("*"));
